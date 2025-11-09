f0b9e12ce3ebe264143fdb00e4cca110428943a5
ruchitha <ruchitha.k2007@gmail.com>
2025-11-09 19:01:09 +0530

    Fix CORS for Render frontend

--- server/app.js
+++ server/app.js
@@ -7,10 +7,22 @@ const cookieParser=require('cookie-parser')
 const authRoute=require('./routes/authRoute')
 
 connectDB();
+const allowedOrigins = [
+    'http://localhost:3000', // local dev
+    'https://to-do-list-app-git-main-ruchithas-projects-1b72de52.vercel.app' // your Vercel frontend
+];
+
 app.use(cors({
-    origin: '*',
+    origin: function(origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
+
 
 
 app.use(express.json());
